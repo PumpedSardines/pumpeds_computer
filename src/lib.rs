d896258d1f2cbba52fdaa4b7008231f1ccbfcf42
Fritiof Rusck <fritiof@rusck.se>
2024-02-01 15:05:07 +0100

    feat: restructure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::collections::HashMap;
 
 enum Op {
@@ -490,4 +491,74 @@ impl Instruction {
             }
         }
     }
+}
+
+enum CodeType {
+    Instruction(Instruction),
+    Label(String),
+}
+
+fn parse_code(code: &String) -> Vec<CodeType> {
+    let mut instructions: Vec<CodeType> = Vec::new();
+    let re = Regex::new(r";.+$").unwrap();
+
+    for line in code.lines() {
+        let line = re.replace(line, "");
+        let line = line.trim();
+        let mut parts = line.split_whitespace();
+        let instruction = parts.next();
+
+        if instruction.is_none() {
+            continue;
+        }
+
+        let instruction = instruction.unwrap();
+
+        if instruction.ends_with(":") {
+            instructions.push(CodeType::Label(
+                instruction[..instruction.len() - 1].to_string(),
+            ));
+            continue;
+        }
+
+        Instruction::from_line(&line.to_string())
+            .unwrap()
+            .map(|i| instructions.push(CodeType::Instruction(i)));
+    }
+
+    instructions
+}
+
+fn assemble(instructions: Vec<CodeType>) -> Vec<(u32, u32, u32, u32)> {
+    let mut labels: HashMap<String, u32> = HashMap::new();
+    let mut bytes = vec![];
+
+    let mut i = 0;
+    for instruction in &instructions {
+        match instruction {
+            CodeType::Instruction(_) => {
+                i += 1;
+            }
+            CodeType::Label(l) => {
+                labels.insert(l.to_string(), (i as u32) * 4);
+            }
+        }
+    }
+
+    for instruction in instructions {
+        match instruction {
+            CodeType::Instruction(i) => {
+                let data = i.to_byte_code(&labels).unwrap();
+
+                bytes.push(data);
+            }
+            CodeType::Label(_) => { /* Do nothing */ }
+        }
+    }
+
+    bytes
+}
+
+pub fn compile(code: &String) -> Vec<(u32, u32, u32, u32)> {
+    assemble(parse_code(code))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,12 @@
-use compiler::Instruction;
-use regex::Regex;
-use std::collections::HashMap;
-
-const CODE: &'static str = include_str!("robot_racing.asm");
-
-enum CodeType {
-    Instruction(Instruction),
-    Label(String),
-}
-
-fn parse_code(code: String) -> Vec<CodeType> {
-    let mut instructions: Vec<CodeType> = Vec::new();
-    let re = Regex::new(r";.+$").unwrap();
-
-    for line in code.lines() {
-        let line = re.replace(line, "");
-        let line = line.trim();
-        let mut parts = line.split_whitespace();
-        let instruction = parts.next();
-
-        if instruction.is_none() {
-            continue;
-        }
-
-        let instruction = instruction.unwrap();
-
-        if instruction.ends_with(":") {
-            instructions.push(CodeType::Label(
-                instruction[..instruction.len() - 1].to_string(),
-            ));
-            continue;
-        }
-
-        Instruction::from_line(&line.to_string())
-            .unwrap()
-            .map(|i| instructions.push(CodeType::Instruction(i)));
-    }
-
-    instructions
-}
-
-fn assemble(instructions: Vec<CodeType>) -> Vec<(u32, u32, u32, u32)> {
-    let mut labels: HashMap<String, u32> = HashMap::new();
-    let mut bytes = vec![];
-
-    let mut i = 0;
-    for instruction in &instructions {
-        match instruction {
-            CodeType::Instruction(_) => {
-                i += 1;
-            }
-            CodeType::Label(l) => {
-                labels.insert(l.to_string(), (i as u32) * 4);
-            }
-        }
-    }
-
-    for instruction in instructions {
-        match instruction {
-            CodeType::Instruction(i) => {
-                let data = i.to_byte_code(&labels).unwrap();
-
-                bytes.push(data);
-            }
-            CodeType::Label(_) => { /* Do nothing */ }
-        }
-    }
-
-    bytes
-}
+use compiler::compile;
+use std::env;
+use std::fs;
 
 fn main() {
-    let output = assemble(parse_code(CODE.to_string()))
+    let args: Vec<String> = env::args().collect();
+    let code = fs::read_to_string(&args[1]).expect("Unable to read file");
+
+    let output = compile(&code)
         .into_iter()
         .map(|(a, b, c, d)| format!("{:#010x} {:#010x} {:#010x} {:#010x}", a, b, c, d))
         .collect::<Vec<String>>()
